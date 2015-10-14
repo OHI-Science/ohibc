777b41366677a193123a44a4635d0bad52651274
jules32 <jstewart@nceas.ucsb.edu>
2015-10-14 11:30:42 -0700

    calculated scores from commit pushing draft branch no travis
    Former-commit-id: 1a354e28a960ec9849fc0a293dace0e2214894b3

---
+++ region2016/calculate_scores.R
@@ -0,0 +1,18 @@
+# load required libraries
+suppressWarnings(require(ohicore))
+
+# set working directory to the scenario directory, ie containing conf and layers directories
+setwd('/Users/jstewart/github/clip-n-ship/ohibc/region2016')
+
+# load scenario configuration
+conf = Conf('conf')
+
+# run checks on scenario layers
+CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
+
+# load scenario layers
+layers = Layers('layers.csv', 'layers')
+
+# calculate scenario scores
+scores = CalculateAll(conf, layers, debug=F)
+write.csv(scores, 'scores.csv', na='', row.names=F)
---
+++ region2016/launch_app_code.R
@@ -0,0 +1,3 @@
+require(methods)
+suppressWarnings(require(ohicore))
+launch_app()
