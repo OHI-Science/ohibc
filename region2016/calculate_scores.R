8e667227293c8ca6f2a8ed10472f3cdd5ba5f032
Julia Stewart Lowndes <jstewart@nceas.ucsb.edu>
2015-10-28 09:54:02 -0700

    add merge branching code

--- region2016/calculate_scores.R
+++ region2016/calculate_scores.R
@@ -16,3 +16,21 @@ layers = Layers('layers.csv', 'layers')
 # calculate scenario scores
 scores = CalculateAll(conf, layers, debug=F)
 write.csv(scores, 'scores.csv', na='', row.names=F)
+
+# merge to published branch (to display on app). Make sure all local work is committed.
+merge_branches = F #change to T, or run line 33-41.
+
+if (merge_branches) {
+  # switch to draft branch and get latest
+  system('git checkout draft; git pull')
+
+  # merge published with the draft branch
+  system('git checkout published')
+  system('git merge draft')
+  system('git push origin published')
+
+  # switch to draft branch and get latest
+  system('git checkout draft; git pull')
+}
+
+
