8e4df96bae9566dad630bed19bca305bcae6fcea
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-10-28 17:45:30 -0700

    trying to figure out the image BS
    Former-commit-id: c3732d3d909eb67519bbfaa99698572b5ab70b95

--- src/R/common.R
+++ src/R/common.R
@@ -7,14 +7,14 @@ dir_neptune_data = c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
 # stop if directory doesn't exist
 if (!file.exists(sprintf('%s/',dir_neptune_data))){
   stop(sprintf("The directory for variable dir_neptune_data set in src/R/common.R does not exist. Do you need to mount %s?", dir_neptune_data))
-  
+
 }
 
 # install (if necessary) and load commonly used libraries
 packages <- c('dplyr', 'tidyr', 'stringr', 'readr', 'ggplot2')
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   cat(sprintf("Installing %s\n", setdiff(packages, rownames(installed.packages()))))
-  install.packages(setdiff(packages, rownames(installed.packages())))  
+  install.packages(setdiff(packages, rownames(installed.packages())))
 }
 
 library(dplyr)
@@ -23,3 +23,5 @@ library(stringr)
 library(readr)
 library(ggplot2)
 rm(packages)
+
+
