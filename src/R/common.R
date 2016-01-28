789a640223990a1e622fe88eb077ef1da41fed58
Casey O'Hara <ohara@nceas.ucsb.edu>
2016-01-28 12:15:18 -0800

    uploading to use on Neptune
    Former-commit-id: aae73eb4fe94b6c04eee202e7339ffc4ed7be468
    Former-commit-id: 967a67b8033066fac419e91242331ba5f3a4c293

--- src/R/common.R
+++ src/R/common.R
@@ -4,9 +4,9 @@ dir_neptune_data = c('Windows' = '//neptune.nceas.ucsb.edu/data_edit',
                      'Darwin'  = '/Volumes/data_edit',
                      'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
 
-# stop if directory doesn't exist
+# WARN rather than stop if directory doesn't exist
 if (!file.exists(sprintf('%s/',dir_neptune_data))){
-  stop(sprintf("The directory for variable dir_neptune_data set in src/R/common.R does not exist. Do you need to mount %s?", dir_neptune_data))
+  warning(sprintf("The directory for variable dir_neptune_data set in src/R/common.R does not exist. Do you need to mount %s?", dir_neptune_data))
 
 }
 
