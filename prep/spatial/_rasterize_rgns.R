9dd4afe1b92fb5fb7f16703920d35101b0944a72
Casey O'Hara <ohara@nceas.ucsb.edu>
2016-07-15 17:26:12 -0700

    cleaning up - ditching the rmd_testing folder (put into provRmd) creating/updating shapes and rasters for buffer regions creating habitat folder and processing wetland rasters

--- prep/spatial/_rasterize_rgns.R
+++ prep/spatial/_rasterize_rgns.R
@@ -5,14 +5,29 @@ library(gdalUtils)
 library(raster)
 
 ### file path for ohibc_rgn and ohibc_land
-rgn_shp_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn.shp'
+# rgn_shp_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn.shp'
+rgn_shp_file  <- '~/github/ohibc/prep/spatial/ohibc_inland_2km.shp'
+
+x <- readOGR(dirname(rgn_shp_file) %>% path.expand(), basename(rgn_shp_file) %>% str_replace('.shp', ''))
+x@data
+x@data <- x@data %>% select(rgn_id = rgn_id_2, rgn_name = rgn_name_2, rgn_code = rgn_code_2)
+writeOGR(x,
+         dirname(rgn_shp_file) %>% path.expand(), basename(rgn_shp_file) %>% str_replace('.shp', ''),
+         overwrite_layer = TRUE,
+         driver = 'ESRI Shapefile')
 
 ### output for rasters
-rgn_rast_tmp_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_rast_tmp_1000m.tif'
-rgn_raster_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_raster_1000m.tif'
+# rgn_rast_tmp_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_raster_tmp_250m.tif'
+# rgn_rast_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_raster_250m.tif'
+# rgn_rast_tmp_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_raster_tmp_100m.tif'
+# rgn_rast_file  <- '~/github/ohibc/prep/spatial/ohibc_rgn_raster_100m.tif'
+# rgn_rast_tmp_file  <- '~/github/ohibc/prep/spatial/ohibc_inland_1km_raster_tmp_100m.tif'
+# rgn_rast_file  <- '~/github/ohibc/prep/spatial/ohibc_inland_1km_raster_100m.tif'
+rgn_rast_tmp_file  <- '~/github/ohibc/prep/spatial/ohibc_inland_2km_raster_tmp_100m.tif'
+rgn_rast_file  <- '~/github/ohibc/prep/spatial/ohibc_inland_2km_raster_100m.tif'
 
 base_te <- c(154000,168000,1876000,1741000)
-res_1km <- c(1000, 1000)
+res <- c(100, 100)
 
 rast_rgn <- gdalUtils::gdal_rasterize(
   src_datasource = path.expand(rgn_shp_file),
@@ -21,16 +36,16 @@ rast_rgn <- gdalUtils::gdal_rasterize(
   a_nodata = NA,
   # at = TRUE,
   te = base_te,
-  tr = res_1km,
+  tr = res,
   output_Raster = TRUE)
 ### this saves an 20+ MB file; But raster::rasterize is really slow.
 
 ### writeRaster to write a compressed version in final location?
 
-writeRaster(rast_rgn, rgn_raster_file, overwrite = TRUE)
+writeRaster(rast_rgn, rgn_rast_file, overwrite = TRUE)
 unlink(rgn_rast_tmp_file) ### delete temp
 
-rast_rgn <- raster(rgn_raster_file)
+rast_rgn <- raster(rgn_rast_file)
 plot(rast_rgn)
 
 
--- rmd_testing/1a_gapfill.R
+++
@@ -1,8 +0,0 @@
-
-table1_file <- file.path(dir_test, 'table1.csv')
-table1      <- read_csv(table1_file)
-
-table1a_file <- file.path(dir_test, 'table1a.csv')
-write_csv(table1, table1a_file)
-
-
