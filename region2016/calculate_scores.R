83b4e0d193c00339f646e60fc63efbdf8994568e
Julia Stewart Lowndes <jstewart@nceas.ucsb.edu>
2016-09-15 13:13:29 -0700

    fixed configure_toolbox call

--- region2016/calculate_scores.R
+++ region2016/calculate_scores.R
@@ -12,7 +12,7 @@
 ## in functions.R is a good workflow.
 
 ## run the configure_toolbox.r script to check configuration
-source('~/github/ohibc/region2016/calculate_scores.r')
+source('~/github/ohibc/region2016/configure_toolbox.r')
 
 ## calculate scenario scores
 scores = ohicore::CalculateAll(conf, layers)
